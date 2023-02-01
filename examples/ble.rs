4aec6b8e0a29d1d67b1882ba06464a2f8637e5a7
bjoernQ <bjoern.quentin@mobile-j.de>
2023-02-01 10:00:43 +0100

    Update BLE example

--- examples/ble.rs
+++ examples/ble.rs
@@ -16,7 +16,7 @@ use bleps::{
     ad_structure::{
         create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
     },
-    attribute_server::{AttributeServer, WorkResult},
+    attribute_server::{AttributeServer, NotificationData, WorkResult},
     Ble, HciConnector,
 };
 use bleps_macros::gatt;
@@ -28,7 +28,7 @@ use hal::{
     clock::{ClockControl, CpuClock},
     peripherals::*,
     prelude::*,
-    Rng, Rtc,
+    Rng, Rtc, IO,
 };
 
 #[cfg(any(feature = "esp32", feature = "esp32s3"))]
@@ -79,6 +79,14 @@ fn main() -> ! {
         initialize(timg1.timer0, Rng::new(peripherals.RNG), &clocks).unwrap();
     }
 
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    #[cfg(any(feature = "esp32", feature = "esp32s2", feature = "esp32s3"))]
+    let button = io.pins.gpio0.into_pull_down_input();
+    #[cfg(any(feature = "esp32c2", feature = "esp32c3"))]
+    let button = io.pins.gpio9.into_pull_down_input();
+
+    let mut debounce_cnt = 500;
+
     loop {
         let connector = BleConnector {};
         let hci = HciConnector::new(connector, esp_wifi::current_millis);
@@ -105,12 +113,19 @@ fn main() -> ! {
 
         println!("started advertising");
 
-        let mut rf = || Data::new(b"Hello Bare-Metal BLE");
-        let mut wf = |data: Data| {
-            println!("RECEIVED: {:x?}", data.to_slice());
+        let mut rf = || &b"Hello Bare-Metal BLE"[..];
+        let mut wf = |offset: u16, data: &[u8]| {
+            println!("RECEIVED: {} {:x?}", offset, data);
         };
 
-        let mut wf2 = |_data| {};
+        let mut wf2 = |offset: u16, data: &[u8]| {
+            println!("RECEIVED: {} {:x?}", offset, data);
+        };
+
+        let mut rf3 = || &b"Hola!"[..];
+        let mut wf3 = |offset: u16, data: &[u8]| {
+            println!("RECEIVED: Offset {}, data {:x?}", offset, data);
+        };
 
         gatt!([service {
             uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
@@ -124,13 +139,43 @@ fn main() -> ! {
                     uuid: "957312e0-2354-11eb-9f10-fbc30a62cf38",
                     write: wf2,
                 },
+                characteristic {
+                    name: "my_characteristic",
+                    uuid: "987312e0-2354-11eb-9f10-fbc30a62cf38",
+                    notify: true,
+                    read: rf3,
+                    write: wf3,
+                },
             ],
         },]);
 
         let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes);
 
         loop {
-            match srv.do_work() {
+            let mut notification = None;
+
+            if button.is_low().unwrap() && debounce_cnt > 0 {
+                debounce_cnt -= 1;
+                if debounce_cnt == 0 {
+                    if let Some(cccd) =
+                        srv.get_characteristic_value(my_characteristic_notify_enable_handle)
+                    {
+                        // if notifications enabled
+                        if cccd[0] == 1 {
+                            notification = Some(NotificationData::new(
+                                my_characteristic_handle,
+                                &b"Notification"[..],
+                            ));
+                        }
+                    }
+                }
+            };
+
+            if button.is_high().unwrap() {
+                debounce_cnt = 500;
+            }
+
+            match srv.do_work_with_notification(notification) {
                 Ok(res) => {
                     if let WorkResult::GotDisconnected = res {
                         break;
