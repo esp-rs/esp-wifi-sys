49602641c527f0948adce29a4ebfcaa4326f47ea
Dániel Buga <bugadani@gmail.com>
2023-10-20 11:49:01 +0200

    Split up link state waking into two branches

--- esp-wifi/src/wifi/os_adapter.rs
+++ esp-wifi/src/wifi/os_adapter.rs
@@ -893,14 +893,16 @@ pub unsafe extern "C" fn event_post(
     event.waker().wake();
 
     #[cfg(feature = "embassy-net")]
-    if matches!(
-        event,
-        WifiEvent::StaConnected
-            | WifiEvent::StaDisconnected
-            | WifiEvent::ApStart
-            | WifiEvent::ApStop
-    ) {
-        crate::wifi::embassy::LINK_STATE.wake();
+    match event {
+        WifiEvent::StaConnected | WifiEvent::StaDisconnected => {
+            crate::wifi::embassy::LINK_STATE.wake();
+        }
+
+        WifiEvent::ApStart | WifiEvent::ApStop => {
+            crate::wifi::embassy::LINK_STATE.wake();
+        }
+
+        _ => {}
     }
 
     memory_fence();
