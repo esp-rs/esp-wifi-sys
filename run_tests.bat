bc188691a39628a2b2dc34b5c57e9be7f83599a1
Juraj Sadel <juraj.sadel@espressif.com>
2023-11-27 11:42:27 +0800

    Add H2 into test scripts

--- run_tests.bat
+++ run_tests.bat
@@ -71,8 +71,11 @@ copy ..\target\riscv32imac-unknown-none-elf\release\examples\open_access_point .
 copy ..\target\riscv32imac-unknown-none-elf\release\examples\test_connect ..\tmp\esp32c6
 copy ..\target\riscv32imac-unknown-none-elf\release\examples\test_ble ..\tmp\esp32c6
 
+cargo +nightly build --release --example test_ble --target riscv32imac-unknown-none-elf --no-default-features --features esp32h2,esp32h2-hal/default,esp32h2-hal/embassy-time-timg0,ble
+copy ..\target\riscv32imac-unknown-none-elf\release\examples\test_ble ..\tmp\esp32h2
+
 cd ..\tmp
-echo "Connect ESP32, ESP32-C2, ESP32-C3, ESP32-C6"
+echo "Connect ESP32, ESP32-C2, ESP32-C3, ESP32-C6, ESP32-H2"
 pause
 esp-testrun --esp32=esp32 --esp32c2=esp32c2 --esp32c3=esp32c3 --esp32c6=esp32c6
 
