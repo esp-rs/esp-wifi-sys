23b0505445ec337b872b0385d6d49bafcde81071
Björn Quentin <bjoernQ@users.noreply.github.com>
2024-01-29 10:17:52 +0100

    Prepare release 0.3.0 (#424)
    * Bump esp-wifi-sys
    
    * Fix code warning
    
    * Fix test script
    
    * Bump esp-wifi
    
    * Prepare CHANGELOG.md for next release cycle

--- run_tests.bat
+++ run_tests.bat
@@ -10,8 +10,8 @@ mkdir tmp\esp32h2
 cd esp-wifi
 cargo +esp build --release --example esp_now_broadcaster --target xtensa-esp32-none-elf --features esp32,esp32-hal/default,esp32-hal/embassy-time-timg0,esp32-hal/embassy-executor-thread,wifi,esp-now
 cargo +esp build --release --example test_esp_now --target xtensa-esp32-none-elf --features esp32,esp32-hal/default,esp32-hal/embassy-time-timg0,esp32-hal/embassy-executor-thread,wifi,esp-now
-cargo +esp build --release --example open_access_point --target xtensa-esp32-none-elf --features esp32,esp32-hal/default,esp32-hal/embassy-time-timg0,esp32-hal/embassy-executor-thread,wifi,esp-now
-cargo +esp build --release --example test_connect --target xtensa-esp32-none-elf --features esp32,esp32-hal/default,esp32-hal/embassy-time-timg0,esp32-hal/embassy-executor-thread,wifi,esp-now
+cargo +esp build --release --example open_access_point --target xtensa-esp32-none-elf --features esp32,esp32-hal/default,esp32-hal/embassy-time-timg0,esp32-hal/embassy-executor-thread,wifi,esp-now,utils,smoltcp,tcp
+cargo +esp build --release --example test_connect --target xtensa-esp32-none-elf --features esp32,esp32-hal/default,esp32-hal/embassy-time-timg0,esp32-hal/embassy-executor-thread,wifi,esp-now,utils,smoltcp,tcp
 cargo +esp build --release --example test_ble --target xtensa-esp32-none-elf --features esp32,esp32-hal/default,esp32-hal/embassy-time-timg0,esp32-hal/embassy-executor-thread,ble
 copy ..\target\xtensa-esp32-none-elf\release\examples\esp_now_broadcaster ..\tmp\esp32
 copy ..\target\xtensa-esp32-none-elf\release\examples\test_esp_now ..\tmp\esp32
@@ -21,8 +21,8 @@ copy ..\target\xtensa-esp32-none-elf\release\examples\test_ble ..\tmp\esp32
 
 cargo +esp build --release --example esp_now_broadcaster --target xtensa-esp32s2-none-elf --features esp32s2,esp32s2-hal/default,esp32s2-hal/embassy-time-timg0,esp32s2-hal/embassy-executor-thread,wifi,esp-now
 cargo +esp build --release --example test_esp_now --target xtensa-esp32s2-none-elf --features esp32s2,esp32s2-hal/default,esp32s2-hal/embassy-time-timg0,esp32s2-hal/embassy-executor-thread,wifi,esp-now
-cargo +esp build --release --example open_access_point --target xtensa-esp32s2-none-elf --features esp32s2,esp32s2-hal/default,esp32s2-hal/embassy-time-timg0,esp32s2-hal/embassy-executor-thread,wifi,esp-now
-cargo +esp build --release --example test_connect --target xtensa-esp32s2-none-elf --features esp32s2,esp32s2-hal/default,esp32s2-hal/embassy-time-timg0,esp32s2-hal/embassy-executor-thread,wifi,esp-now
+cargo +esp build --release --example open_access_point --target xtensa-esp32s2-none-elf --features esp32s2,esp32s2-hal/default,esp32s2-hal/embassy-time-timg0,esp32s2-hal/embassy-executor-thread,wifi,esp-now,utils,smoltcp,tcp
+cargo +esp build --release --example test_connect --target xtensa-esp32s2-none-elf --features esp32s2,esp32s2-hal/default,esp32s2-hal/embassy-time-timg0,esp32s2-hal/embassy-executor-thread,wifi,esp-now,utils,smoltcp,tcp
 copy ..\target\xtensa-esp32s2-none-elf\release\examples\esp_now_broadcaster ..\tmp\esp32s2
 copy ..\target\xtensa-esp32s2-none-elf\release\examples\test_esp_now ..\tmp\esp32s2
 copy ..\target\xtensa-esp32s2-none-elf\release\examples\open_access_point ..\tmp\esp32s2
@@ -30,8 +30,8 @@ copy ..\target\xtensa-esp32s2-none-elf\release\examples\test_connect ..\tmp\esp3
 
 cargo +esp build --release --example esp_now_broadcaster --target xtensa-esp32s3-none-elf --features esp32s3,esp32s3-hal/default,esp32s3-hal/embassy-time-timg0,esp32s3-hal/embassy-executor-thread,wifi,esp-now
 cargo +esp build --release --example test_esp_now --target xtensa-esp32s3-none-elf --features esp32s3,esp32s3-hal/default,esp32s3-hal/embassy-time-timg0,esp32s3-hal/embassy-executor-thread,wifi,esp-now
-cargo +esp build --release --example open_access_point --target xtensa-esp32s3-none-elf --features esp32s3,esp32s3-hal/default,esp32s3-hal/embassy-time-timg0,esp32s3-hal/embassy-executor-thread,wifi,esp-now
-cargo +esp build --release --example test_connect --target xtensa-esp32s3-none-elf --features esp32s3,esp32s3-hal/default,esp32s3-hal/embassy-time-timg0,esp32s3-hal/embassy-executor-thread,wifi,esp-now
+cargo +esp build --release --example open_access_point --target xtensa-esp32s3-none-elf --features esp32s3,esp32s3-hal/default,esp32s3-hal/embassy-time-timg0,esp32s3-hal/embassy-executor-thread,wifi,esp-now,utils,smoltcp,tcp
+cargo +esp build --release --example test_connect --target xtensa-esp32s3-none-elf --features esp32s3,esp32s3-hal/default,esp32s3-hal/embassy-time-timg0,esp32s3-hal/embassy-executor-thread,wifi,esp-now,utils,smoltcp,tcp
 cargo +esp build --release --example test_ble --target xtensa-esp32s3-none-elf --features esp32s3,esp32s3-hal/default,esp32s3-hal/embassy-time-timg0,esp32s3-hal/embassy-executor-thread,ble
 copy ..\target\xtensa-esp32s3-none-elf\release\examples\esp_now_broadcaster ..\tmp\esp32s3
 copy ..\target\xtensa-esp32s3-none-elf\release\examples\test_esp_now ..\tmp\esp32s3
@@ -41,8 +41,8 @@ copy ..\target\xtensa-esp32s3-none-elf\release\examples\test_ble ..\tmp\esp32s3
 
 cargo +nightly build --release --example esp_now_broadcaster --target riscv32imc-unknown-none-elf --features esp32c2,esp32c2-hal/default,esp32c2-hal/embassy-time-timg0,wifi,esp-now
 cargo +nightly build --release --example test_esp_now --target riscv32imc-unknown-none-elf --features esp32c2,esp32c2-hal/default,esp32c2-hal/embassy-time-timg0,wifi,esp-now
-cargo +nightly build --release --example open_access_point --target riscv32imc-unknown-none-elf --features esp32c2,esp32c2-hal/default,esp32c2-hal/embassy-time-timg0,wifi,esp-now
-cargo +nightly build --release --example test_connect --target riscv32imc-unknown-none-elf --features esp32c2,esp32c2-hal/default,esp32c2-hal/embassy-time-timg0,wifi,esp-now
+cargo +nightly build --release --example open_access_point --target riscv32imc-unknown-none-elf --features esp32c2,esp32c2-hal/default,esp32c2-hal/embassy-time-timg0,wifi,esp-now,utils,smoltcp,tcp
+cargo +nightly build --release --example test_connect --target riscv32imc-unknown-none-elf --features esp32c2,esp32c2-hal/default,esp32c2-hal/embassy-time-timg0,wifi,esp-now,utils,smoltcp,tcp
 cargo +nightly build --release --example test_ble --target riscv32imc-unknown-none-elf --features esp32c2,esp32c2-hal/default,esp32c2-hal/embassy-time-timg0,ble
 copy ..\target\riscv32imc-unknown-none-elf\release\examples\esp_now_broadcaster ..\tmp\esp32c2
 copy ..\target\riscv32imc-unknown-none-elf\release\examples\test_esp_now ..\tmp\esp32c2
@@ -52,8 +52,8 @@ copy ..\target\riscv32imc-unknown-none-elf\release\examples\test_ble ..\tmp\esp3
 
 cargo +nightly build --release --example esp_now_broadcaster --target riscv32imc-unknown-none-elf --features esp32c3,esp32c3-hal/default,esp32c3-hal/embassy-time-timg0,wifi,esp-now
 cargo +nightly build --release --example test_esp_now --target riscv32imc-unknown-none-elf --features esp32c3,esp32c3-hal/default,esp32c3-hal/embassy-time-timg0,wifi,esp-now
-cargo +nightly build --release --example open_access_point --target riscv32imc-unknown-none-elf --features esp32c3,esp32c3-hal/default,esp32c3-hal/embassy-time-timg0,wifi,esp-now
-cargo +nightly build --release --example test_connect --target riscv32imc-unknown-none-elf --features esp32c3,esp32c3-hal/default,esp32c3-hal/embassy-time-timg0,wifi,esp-now
+cargo +nightly build --release --example open_access_point --target riscv32imc-unknown-none-elf --features esp32c3,esp32c3-hal/default,esp32c3-hal/embassy-time-timg0,wifi,esp-now,utils,smoltcp,tcp
+cargo +nightly build --release --example test_connect --target riscv32imc-unknown-none-elf --features esp32c3,esp32c3-hal/default,esp32c3-hal/embassy-time-timg0,wifi,esp-now,utils,smoltcp,tcp
 cargo +nightly build --release --example test_ble --target riscv32imc-unknown-none-elf --features esp32c3,esp32c3-hal/default,esp32c3-hal/embassy-time-timg0,ble
 copy ..\target\riscv32imc-unknown-none-elf\release\examples\esp_now_broadcaster ..\tmp\esp32c3
 copy ..\target\riscv32imc-unknown-none-elf\release\examples\test_esp_now ..\tmp\esp32c3
@@ -63,8 +63,8 @@ copy ..\target\riscv32imc-unknown-none-elf\release\examples\test_ble ..\tmp\esp3
 
 cargo +nightly build --release --example esp_now_broadcaster --target riscv32imac-unknown-none-elf --features esp32c6,esp32c6-hal/default,esp32c6-hal/embassy-time-timg0,wifi,esp-now
 cargo +nightly build --release --example test_esp_now --target riscv32imac-unknown-none-elf --features esp32c6,esp32c6-hal/default,esp32c6-hal/embassy-time-timg0,wifi,esp-now
-cargo +nightly build --release --example open_access_point --target riscv32imac-unknown-none-elf --features esp32c6,esp32c6-hal/default,esp32c6-hal/embassy-time-timg0,wifi,esp-now
-cargo +nightly build --release --example test_connect --target riscv32imac-unknown-none-elf --features esp32c6,esp32c6-hal/default,esp32c6-hal/embassy-time-timg0,wifi,esp-now
+cargo +nightly build --release --example open_access_point --target riscv32imac-unknown-none-elf --features esp32c6,esp32c6-hal/default,esp32c6-hal/embassy-time-timg0,wifi,esp-now,utils,smoltcp,tcp
+cargo +nightly build --release --example test_connect --target riscv32imac-unknown-none-elf --features esp32c6,esp32c6-hal/default,esp32c6-hal/embassy-time-timg0,wifi,esp-now,utils,smoltcp,tcp
 cargo +nightly build --release --example test_ble --target riscv32imac-unknown-none-elf --features esp32c6,esp32c6-hal/default,esp32c6-hal/embassy-time-timg0,ble
 copy ..\target\riscv32imac-unknown-none-elf\release\examples\esp_now_broadcaster ..\tmp\esp32c6
 copy ..\target\riscv32imac-unknown-none-elf\release\examples\test_esp_now ..\tmp\esp32c6
