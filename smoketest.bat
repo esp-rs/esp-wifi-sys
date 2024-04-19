01d88002977e6c3165e5cd05b6809d5306d77dec
Björn Quentin <bjoernQ@users.noreply.github.com>
2024-04-19 08:29:30 +0200

    Use esp-hal 0.17.0 (#456)

--- smoketest.bat
+++ smoketest.bat
@@ -13,18 +13,28 @@ echo.
 echo Connect ESP32-C3
 pause
 cargo +nightly esp32c3 --example ble --release --features "ble"
+pause
 cargo +nightly esp32c3 --example embassy_ble --release --features "async,ble"
+pause
 cargo +nightly esp32c3 --example dhcp --release --features "wifi"
+pause
 cargo +nightly esp32c3 --example static_ip --release --features "wifi"
+pause
 cargo +nightly esp32c3 --example embassy_dhcp --release --features "async,wifi,embassy-net"
+pause
 echo [esp-wifi] >..\cfg.toml
 echo heap_size = 70000 >>..\cfg.toml
 cargo +nightly esp32c3 --example coex --release --features "wifi,ble,coex"
+pause
 del /q ..\cfg.toml
 cargo +nightly esp32c3 --example esp_now --release --features "esp-now"
+pause
 cargo +nightly esp32c3 --example embassy_esp_now --release --features "async,esp-now"
+pause
 cargo +nightly esp32c3 --example access_point --release --features "wifi"
+pause
 cargo +nightly esp32c3 --example embassy_access_point --release --features "async,wifi,embassy-net"
+pause
 
 set CARGO_PROFILE_RELEASE_OPT_LEVEL=3
 set CARGO_PROFILE_RELEASE_LTO=off
@@ -32,15 +42,25 @@ echo.
 echo Connect ESP32
 pause
 cargo +esp esp32 --example ble --release --features "ble"
+pause
 cargo +esp esp32 --example embassy_ble --release --features "async,ble"
+pause
 cargo +esp esp32 --example dhcp --release --features "wifi"
+pause
 cargo +esp esp32 --example static_ip --release --features "wifi"
+pause
 cargo +esp esp32 --example embassy_dhcp --release --features "async,wifi,embassy-net"
+pause
 cargo +esp esp32 --example coex --release --features "wifi,ble,coex"
+pause
 cargo +esp esp32 --example esp_now --release --features "esp-now"
+pause
 cargo +esp esp32 --example embassy_esp_now --release --features "async,esp-now"
+pause
 cargo +esp esp32 --example access_point --release --features "wifi"
+pause
 cargo +esp esp32 --example embassy_access_point --release --features "async,wifi,embassy-net"
+pause
 
 set CARGO_PROFILE_RELEASE_OPT_LEVEL=3
 set CARGO_PROFILE_RELEASE_LTO=off
@@ -48,15 +68,25 @@ echo.
 echo Connect ESP32-S3
 pause
 cargo +esp esp32s3 --example ble --release --features "ble"
+pause
 cargo +esp esp32s3 --example embassy_ble --release --features "async,ble"
+pause
 cargo +esp esp32s3 --example dhcp --release --features "wifi"
+pause
 cargo +esp esp32s3 --example static_ip --release --features "wifi"
+pause
 cargo +esp esp32s3 --example embassy_dhcp --release --features "async,wifi,embassy-net"
+pause
 cargo +esp esp32s3 --example coex --release --features "wifi,ble,coex"
+pause
 cargo +esp esp32s3 --example esp_now --release --features "esp-now"
+pause
 cargo +esp esp32s3 --example embassy_esp_now --release --features "async,esp-now"
+pause
 cargo +esp esp32s3 --example access_point --release --features "wifi"
+pause
 cargo +esp esp32s3 --example embassy_access_point --release --features "async,wifi,embassy-net"
+pause
 
 set CARGO_PROFILE_RELEASE_OPT_LEVEL=2
 set CARGO_PROFILE_RELEASE_LTO=off
@@ -64,12 +94,19 @@ echo.
 echo Connect ESP32-S2
 pause
 cargo +esp esp32s2 --example dhcp --release --features "wifi"
+pause
 cargo +esp esp32s2 --example static_ip --release --features "wifi"
+pause
 cargo +esp esp32s2 --example embassy_dhcp --release --features "async,wifi,embassy-net"
+pause
 cargo +esp esp32s2 --example esp_now --release --features "esp-now"
+pause
 cargo +esp esp32s2 --example embassy_esp_now --release --features "async,esp-now"
+pause
 cargo +esp esp32s2 --example access_point --release --features "wifi"
+pause
 cargo +esp esp32s2 --example embassy_access_point --release --features "async,wifi,embassy-net"
+pause
 
 set CARGO_PROFILE_RELEASE_OPT_LEVEL=3
 set CARGO_PROFILE_RELEASE_LTO=false
@@ -77,18 +114,28 @@ echo.
 echo Connect ESP32-C2
 pause
 cargo +nightly esp32c2 --example ble --release --features "ble"
+pause
 cargo +nightly esp32c2 --example embassy_ble --release --features "async,ble"
+pause
 cargo +nightly esp32c2 --example dhcp --release --features "wifi"
+pause
 cargo +nightly esp32c2 --example static_ip --release --features "wifi"
+pause
 cargo +nightly esp32c2 --example embassy_dhcp --release --features "async,wifi,embassy-net"
+pause
 echo [esp-wifi] >..\cfg.toml
 echo heap_size = 70000 >>..\cfg.toml
 cargo +nightly esp32c2 --example coex --release --features "wifi,ble,coex"
+pause
 del /q ..\cfg.toml
 cargo +nightly esp32c2 --example esp_now --release --features "esp-now"
+pause
 cargo +nightly esp32c2 --example embassy_esp_now --release --features "async,esp-now"
+pause
 cargo +nightly esp32c2 --example access_point --release --features "wifi"
+pause
 cargo +nightly esp32c2 --example embassy_access_point --release --features "async,wifi,embassy-net"
+pause
 
 set CARGO_PROFILE_RELEASE_OPT_LEVEL=3
 set CARGO_PROFILE_RELEASE_LTO=off
@@ -96,23 +143,35 @@ echo.
 echo Connect ESP32-C6
 pause
 cargo +nightly esp32c6 --example ble --release --features "ble"
+pause
 cargo +nightly esp32c6 --example embassy_ble --release --features "async,ble"
+pause
 cargo +nightly esp32c6 --example dhcp --release --features "wifi"
+pause
 cargo +nightly esp32c6 --example static_ip --release --features "wifi"
+pause
 cargo +nightly esp32c6 --example embassy_dhcp --release --features "async,wifi,embassy-net"
+pause
 echo [esp-wifi] >..\cfg.toml
 echo heap_size = 80000 >>..\cfg.toml
 echo tx_queue_size = 10 >>..\cfg.toml
 cargo +nightly esp32c6 --example coex --release --features "wifi,ble,coex"
+pause
 del /q ..\cfg.toml
 cargo +nightly esp32c6 --example esp_now --release --features "esp-now"
+pause
 cargo +nightly esp32c6 --example embassy_esp_now --release --features "async,esp-now"
+pause
 cargo +nightly esp32c6 --example access_point --release --features "wifi"
+pause
 cargo +nightly esp32c6 --example embassy_access_point --release --features "async,wifi,embassy-net"
+pause
 
 set CARGO_PROFILE_RELEASE_OPT_LEVEL=3
 echo.
 echo Connect ESP32-H2
 pause
 cargo +nightly esp32h2 --example ble --release --no-default-features --features "ble"
+pause
 cargo +nightly esp32h2 --example embassy_ble --release --no-default-features  --features "async,ble"
+pause
