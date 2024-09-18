3a5cb1112d846184008dba3c240211fc80966827
Scott Mabin <scott@mabez.dev>
2024-09-18 13:47:09 +0100

    Add binary logs feature to esp-wifi-sys

--- esp-wifi-sys/src/lib.rs
+++ esp-wifi-sys/src/lib.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![doc(html_logo_url = "https://avatars.githubusercontent.com/u/46717278")]
+#![cfg_attr(feature = "binary-logs", feature(c_variadic))]
 
 pub mod c_types;
+mod fmt;
 
 #[allow(improper_ctypes)]
 #[cfg_attr(feature = "esp32", path = "include/esp32.rs")]
@@ -12,3 +14,65 @@ pub mod c_types;
 #[cfg_attr(feature = "esp32s2", path = "include/esp32s2.rs")]
 #[cfg_attr(feature = "esp32s3", path = "include/esp32s3.rs")]
 pub mod include;
+
+#[cfg(feature = "binary-logs")]
+mod log {
+    #[no_mangle]
+    pub unsafe extern "C" fn rtc_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn phy_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn coexist_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn net80211_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn pp_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn syslog(_priority: u32, _format: *const u8, _args: ...) {
+        #[allow(clashing_extern_declarations)]
+        extern "C" {
+            fn vsnprintf(buffer: *mut u8, len: usize, fmt: *const u8, args: ...);
+        }
+
+        let mut buf = [0u8; 512];
+        vsnprintf(&mut buf as *mut u8, 512, _format, _args);
+        let res_str = core::ffi::CStr::from_ptr(&buf as *const _ as *const i8);
+        info!("{}", res_str.to_str().unwrap());
+    }
+}
+
+#[cfg(not(feature = "binary-logs"))]
+mod log {
+    #[no_mangle]
+    pub unsafe extern "C" fn rtc_printf(_s: *const u8, _args: *const ()) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn phy_printf(_s: *const u8, _args: *const ()) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn coexist_printf(_s: *const u8, _args: *const ()) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn net80211_printf(_s: *const u8, _args: *const ()) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn pp_printf(_s: *const u8, _args: *const ()) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn syslog(_priority: u32, _format: *const u8, _args: *const ()) {}
+}
