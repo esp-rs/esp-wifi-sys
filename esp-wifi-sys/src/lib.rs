c25752e4715cf5db40b7d21d8c9346ad71edafd6
Scott Mabin <scott@mabez.dev>
2024-09-19 10:29:10 -0700

    Add binary logs feature to esp-wifi-sys (#477)
    * Add binary logs feature to esp-wifi-sys
    
    * made log pub
    
    * use correct size for args

--- esp-wifi-sys/src/lib.rs
+++ esp-wifi-sys/src/lib.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![doc(html_logo_url = "https://avatars.githubusercontent.com/u/46717278")]
+#![cfg_attr(feature = "sys-logs", feature(c_variadic))]
 
 pub mod c_types;
+mod fmt;
 
 #[allow(improper_ctypes)]
 #[cfg_attr(feature = "esp32", path = "include/esp32.rs")]
@@ -12,3 +14,77 @@ pub mod c_types;
 #[cfg_attr(feature = "esp32s2", path = "include/esp32s2.rs")]
 #[cfg_attr(feature = "esp32s3", path = "include/esp32s3.rs")]
 pub mod include;
+
+#[cfg(feature = "sys-logs")]
+pub mod log {
+    #[no_mangle]
+    pub unsafe extern "C" fn rtc_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn phy_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn coexist_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn net80211_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn pp_printf(s: *const u8, args: ...) {
+        syslog(0, s, args);
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn syslog(
+        _priority: u32,
+        format: *const u8,
+        args: core::ffi::VaListImpl,
+    ) {
+        #[allow(clashing_extern_declarations)]
+        extern "C" {
+            fn vsnprintf(buffer: *mut u8, len: usize, fmt: *const u8, args: ...);
+        }
+
+        let mut buf = [0u8; 512];
+        vsnprintf(&mut buf as *mut u8, 512, format, args);
+        let res_str = core::ffi::CStr::from_ptr(&buf as *const _ as *const i8);
+        info!("{}", res_str.to_str().unwrap());
+    }
+}
+
+#[cfg(not(feature = "sys-logs"))]
+pub mod log {
+    #[cfg(target_arch = "riscv32")]
+    type VaargType = *const ();
+    #[cfg(target_arch = "xtensa")]
+    #[repr(C)]
+    pub struct VaListDummy([u32; 3]);
+    #[cfg(target_arch = "xtensa")]
+    type VaargType = VaListDummy;
+
+    #[no_mangle]
+    pub unsafe extern "C" fn rtc_printf(_s: *const u8, _args: VaargType) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn phy_printf(_s: *const u8, _args: VaargType) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn coexist_printf(_s: *const u8, _args: VaargType) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn net80211_printf(_s: *const u8, _args: VaargType) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn pp_printf(_s: *const u8, _args: VaargType) {}
+
+    #[no_mangle]
+    pub unsafe extern "C" fn syslog(_priority: u32, _format: *const u8, _args: VaargType) {}
+}
